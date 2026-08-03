1300a8ac9ed8a6006e62169ad0fc68826faa0d4c
Paula Lucy <u8730576247@gmail.com>
2026-08-03 15:28:44 +0200

    Add rewrite rules and force sync flutter assets

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/vox-rex-lex",
+        destination: "/vox-rex-lex/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
